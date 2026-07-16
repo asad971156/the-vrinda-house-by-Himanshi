ea4aae6fb801129b57ddb751fc2aae7c730a2895
asad971156 <recreationservices.rs@gmail.com>
2026-07-16 19:53:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-const header = document.querySelector("header");
 
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-  link.addEventListener("click", function (e) {
-    const target = document.querySelector(this.getAttribute("href"));
-
-    if (target) {
-      e.preventDefault();
-
-      const topBar = document.querySelector(".top-booking");
-const offset = header.offsetHeight + (topBar ? topBar.offsetHeight : 0) + 15;
-
-      window.scrollTo({
-        top: target.offsetTop - offset,
-        behavior: "smooth"
-      });
-    }
-  });
-});
