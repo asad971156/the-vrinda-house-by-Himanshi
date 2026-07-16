75fe0df404306bb48f5daecdc5a08381ffb92200
asad971156 <recreationservices.rs@gmail.com>
2026-07-16 19:02:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.onclick=e=>{e.preventDefault();document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});});
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if (target) {
+            e.preventDefault();
+
+            const header = document.querySelector('header');
+            const offset = header ? header.offsetHeight : 0;
+
+            window.scrollTo({
+                top: target.offsetTop - offset,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
